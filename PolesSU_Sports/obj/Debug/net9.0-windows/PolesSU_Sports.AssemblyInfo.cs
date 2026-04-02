a9558404dcd6ee5fb8a49f30783d58224f25f2fc
Игнатий Уласовец <ignat041103@gmail.com>
2026-04-02 22:07:14 +0300

    ignore vs

--- PolesSU_Sports/obj/Debug/net9.0-windows/PolesSU_Sports.AssemblyInfo.cs
+++ PolesSU_Sports/obj/Debug/net9.0-windows/PolesSU_Sports.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PolesSU_Sports")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fad8bfd62c1811572fdbeae3b5fa0c6d216cf396")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c603bac899e0e5371ba15a0c4c3588e6b02074b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PolesSU_Sports")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PolesSU_Sports")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
